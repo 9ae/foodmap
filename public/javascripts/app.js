d06b3165668c9ffe7ba03b844f2c4970e5558ee7
Alice Q Wong <alice@pelotoncycle.com>
2015-10-17 18:22:33 -0400

    frontend

---
+++ public/javascripts/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('tofu', ['ngMaterial']);
+
+var map;
+var currentLocation = {'lat': 0, 'lon': 0};
+
+function initMap() {
+
+  if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        	currentLocation.lat = position.coords.latitude;
+        	currentLocation.lon = position.coords.longitude;
+        	  map = new google.maps.Map(document.getElementById('map'), {
+			    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+			    scrollwheel: false,
+			    zoom: 15
+			  });
+        });
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+    
+}
+
+app.directive('ngEnter', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === 13) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.ngEnter, {'event': event});
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+
+app.controller('SearchMapper', function($scope) {
+	$scope.tag = '';
+	
+	$scope.doSearch = function(){
+		console.log($scope.tag);
+	};
+});
